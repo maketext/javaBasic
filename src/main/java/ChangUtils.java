a4350da1ffb2d87864572c9c2afdff33e42079b7
Chang Ho Lee <maketext@naver.com>
2022-04-03 19:10:59 +0900

    no message

--- src/main/java/ChangUtils.java
+++ src/main/java/ChangUtils.java
@@ -1,3 +1,6 @@
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 
 public class ChangUtils {
     public String streamToString(InputStream inputStream) throws IOException {
@@ -28,5 +32,42 @@ public class ChangUtils {
     }
     void wrapNullCallback(Object object, Runnable callbackFunction) {
         if(object == null) callbackFunction.run();
+    }
+
+    private enum Crypto {
+        Algorithm("AES/CBC/PKCS5Padding"),
+        Key("F130C02F55C5C7AABFE51907E4065A7426B52B6".substring(0, 32)),
+        IV(Crypto.Key.value.substring(0, 16));
+
+        private String value;
+        Crypto(String value)
+        {
+            this.value = value;
+        }
+        String get()
+        {
+            return this.value;
+        }
+
+        public String encryptText(String text) throws Exception {
+            Cipher cipher = Cipher.getInstance(Crypto.Algorithm.value);
+            SecretKeySpec keySpec = new SecretKeySpec(Crypto.Key.value.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(Crypto.IV.value.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(encrypted);
+        }
+
+        public String decryptText(String cipherText) throws Exception {
+            Cipher cipher = Cipher.getInstance(Crypto.Algorithm.value);
+            SecretKeySpec keySpec = new SecretKeySpec(Crypto.Key.value.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(Crypto.IV.value.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+            byte[] decrypted = cipher.doFinal(decodedBytes);
+            return new String(decrypted, "UTF-8");
+        }
     }
 }
