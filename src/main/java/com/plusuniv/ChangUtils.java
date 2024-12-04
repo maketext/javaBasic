4523e918eb002d887144d1cedc563d7488441c24
L. Chang <32004044+maketext@users.noreply.github.com>
2024-12-04 21:00:14 +0900

    Update ChangUtils.java

--- src/main/java/com/plusuniv/ChangUtils.java
+++ src/main/java/com/plusuniv/ChangUtils.java
@@ -2,22 +2,86 @@ package com.plusuniv;
 
 import org.springframework.stereotype.Component;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.Period;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Base64;
 
 @Component
 public class ChangUtils {
-    public String streamToString(InputStream inputStream) throws IOException {
+    public MeasureTimeSpan measureMills = new MeasureTimeSpan();
+    public class MeasureTimeSpan {
+        long startTimeMills = 0L;
+        long endTimeMills = 0L;
+        long getTimeMills() {
+            if(endTimeMills - startTimeMills < 0)
+                return 0;
+            return endTimeMills - startTimeMills;
+        }
+        long getTimeSecond() {
+            return getTimeMills() / 1000;
+        }
+        void setTime() {
+            if(startTimeMills == 0L)
+                startTimeMills = Instant.now().toEpochMilli();
+            else
+                endTimeMills = Instant.now().toEpochMilli();
+        }
+    }
+    public void print(String text) {
+        System.out.println(text);
+    }
+    public String dateNow() {
+        LocalDate localDate = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        return localDate.toString();
+    }
+    public String dateTimeNow() {
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return localDateTime.format(formatter);
+    }
+
+    @FunctionalInterface
+    interface FunctionParam {
+        void run() throws Exception;
+    }
+
+    public int tryFunction(FunctionParam function) {
+        try {
+            function.run();
+            return 0;
+        } catch (Exception e) {
+            return 1;
+        }
+    }
+    private FunctionParam fn1, fnJWTValidation;
+    public void setFunction1(FunctionParam fn1) {
+        this.fn1 = fn1;
+    }
+    public void setJWTValidationFunction(FunctionParam fnJWTValidation) {
+        this.fnJWTValidation = fnJWTValidation;
+    }
+    public int tryFunction1() {
+        try {
+            fn1.run();
+            return 0;
+        } catch (Exception e) {
+            return 1;
+        }
+    }
+    public int tryJWTValidation() {
+        try {
+            fnJWTValidation.run();
+            return 0;
+        } catch (Exception e) {
+            return 1;
+        }
+    }
+    public String inputStreamToString(InputStream inputStream) throws IOException {
         ByteArrayOutputStream result = new ByteArrayOutputStream();
         byte[] buffer = new byte[512];
         for (int length; (length = inputStream.read(buffer)) != -1; )
@@ -25,7 +89,7 @@ public class ChangUtils {
         inputStream.close();
         return result.toString(StandardCharsets.UTF_8.name());
     }
-    public void streamToStringUTF_8(OutputStream outputStream, String toWrite) throws IOException {
+    public void stringToOutputStream(OutputStream outputStream, String toWrite) throws IOException {
         outputStream.write(toWrite.getBytes(StandardCharsets.UTF_8));
         outputStream.close();
     }
@@ -33,48 +97,9 @@ public class ChangUtils {
         Period period = Period.between(startDate, endDate);
         period.normalized(); // 1달 1일 == 32일, 31일 둘 다 가능
         return (int) ChronoUnit.DAYS.between(startDate, endDate); // only  32일
-
     }
     public void wrapNullCallback(Object object, Runnable callbackFunction) {
         if(object == null) callbackFunction.run();
     }
 
-    public enum Crypto {
-        INSTANCE("INSTANCE"),
-        Algorithm("AES/CBC/PKCS5Padding"),
-        Key("F130C02F55C5C7AABFE51907E4065A7426B52B6".substring(0, 32)),
-        IV(Crypto.Key.value.substring(0, 16));
-
-        private String value;
-        Crypto(String value)
-        {
-            this.value = value;
-        }
-        String get()
-        {
-            return this.value;
-        }
-        void set(String value) { this.value = value; }
-
-        public String encryptText(String text) throws Exception {
-            Cipher cipher = Cipher.getInstance(Crypto.Algorithm.value);
-            SecretKeySpec keySpec = new SecretKeySpec(Crypto.Key.value.getBytes(), "AES");
-            IvParameterSpec ivParamSpec = new IvParameterSpec(Crypto.IV.value.getBytes());
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
-
-            byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
-            return Base64.getEncoder().encodeToString(encrypted);
-        }
-
-        public String decryptText(String cipherText) throws Exception {
-            Cipher cipher = Cipher.getInstance(Crypto.Algorithm.value);
-            SecretKeySpec keySpec = new SecretKeySpec(Crypto.Key.value.getBytes(), "AES");
-            IvParameterSpec ivParamSpec = new IvParameterSpec(Crypto.IV.value.getBytes());
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
-
-            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
-            byte[] decrypted = cipher.doFinal(decodedBytes);
-            return new String(decrypted, "UTF-8");
-        }
-    }
 }
